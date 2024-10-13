85436b951a55dad9e1440ca3f7fce5e38270fbe3
alicehe2003 <140996846+alicehe2003@users.noreply.github.com>
2024-10-13 14:02:07 -0700

    Implemented message display using EJS

--- app.js
+++ app.js
@@ -2,6 +2,19 @@ const express = require("express");
 const app = express(); 
 const path = require('path'); 
 
+const messages = [
+    {
+      text: "Hi there!",
+      user: "Amando",
+      added: new Date()
+    },
+    {
+      text: "Hello World!",
+      user: "Charles",
+      added: new Date()
+    }
+];
+
 // Set view engine to ejs
 app.set('view engine', 'ejs'); 
 
@@ -10,8 +23,15 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Serve home page 
 app.get('/', (req, res) => {
-    res.render('index'); 
+    res.render('index', 
+        { title: "Mini Messageboard", messages : messages }
+    ); 
 }); 
+
+// Serve new message 
+app.get('/new', (req, res) => {
+    res.render('new'); 
+})
 
 // Serve 404 page for any unknown route 
 app.use((req, res) => {
